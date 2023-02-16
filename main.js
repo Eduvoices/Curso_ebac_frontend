ee48f7c3eb23d1cb8fb70dbf3e518fc7164549b5
Luis Eduardo Ulinski <91352599+Eduvoices@users.noreply.github.com>
2023-02-16 15:02:30 -0300

    adicionando o tratamento de exceções

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
-    const endpoint = 'https://api.github.com/users/ogiansouza'
+    const endpoint = 'https://api.github.com/users/Eduvoices'
 
-    $.ajax(endpoint).done(function(resposta){
+    $.ajax(endpoint)
+        .done(function(resposta){
             const repositorios = resposta.public_repos
             const seguidores = resposta.followers
             const seguindo = resposta.following
@@ -17,5 +18,16 @@ $(document).ready(function(){
             $('#profile-link').append(`<a href="${linkBtn}" class="profile-link">Ver no Github</a>`)
             $('#profile-name').append(nome)
             $('#profile-username').append(login)
+        })
+        .fail(function(){
+            $('#repositorios').append("0")
+            $('#seguidores').append("0")
+            $('#seguindo').append("0")
+            $('#profile-avatar').append(` <img class="profile-avatar" src="https://via.placeholder.com/180x180" alt=".">`)
+            $('#profile-link').append(`<a href="#" class="profile-link">Ver no Github</a>`)
+            $('#profile-name').append("Nome")
+            $('#profile-username').append("login")
+            alert ('Houve um erro na requisição. Confirme se o endpoint informado está correto')
+            throw new Error('Informe o endpoint correto')
         })
 })
