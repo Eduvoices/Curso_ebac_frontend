bf46796e4f48fcad43cbe746da19489170fc843e
Luis Eduardo Ulinski <91352599+Eduvoices@users.noreply.github.com>
2023-03-09 21:35:25 -0300

    Add files via upload

---
+++ exercicio_modulo.ts
@@ -0,0 +1,11 @@
+function multiplicação(fator1: number, fator2: number): number{
+    return fator1 * fator2
+}
+
+console.log(multiplicação(5,8))
+
+function saudação(nome: string): string {
+    return `Olá ${nome}`
+}
+
+console.log(saudação('Gandalf'))
